0d57f4fce6b86fdf9e58def0a2346b0c5e7e23e3
Chanhee Jeong <chan420420@gmail.com>
2022-04-06 10:08:58 +0900

    Initial Commit

---
+++ talent-exchange/talent_exchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  talent_exchangeApp.swift
+//  talent-exchange
+//
+//  Created by Chanhee Jeong on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct talent_exchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
